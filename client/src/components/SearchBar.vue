a3e334c36ff4694297700ffa5319714f1f9d6032
Admin <patrickemaher@yahoo.com>
2020-01-03 21:30:04 -0800

    Adding semantic search bar

--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -1,20 +1,29 @@
 <template>
-    <div>
-        <input @input="onInput" />
-    </div>    
+  <div class="ui menu">
+    <div class="header item">
+      Address Book
+    </div>
+    <div class="right menu">
+        <div class="item">
+            <div class="ui transparent icon input">
+                <input type="text" placeholder="Search..." @input="onInput" />
+                <i class="search link icon"></i>
+            </div>
+        </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'SearchBar',
-    methods: {
-        onInput: function(event) {
-            console.log(event.target.value);
-        }
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      console.log(event.target.value);
     }
-}
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
