7ff98c3c94d77ad815ed2624c49a1b228d2f3e53
Admin <patrickemaher@yahoo.com>
2020-01-07 21:34:22 -0800

    Formatting header

--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="ui menu">
-    <div class="header item">
-      Address Book
-    </div>
+    <h2 class="ui header">
+      <i class="users icon"></i>
+      <div class="content">
+        My Address Book
+      </div>
+    </h2>
     <div class="right menu">
-        <div class="item">
-            <div class="ui transparent icon input">
-                <input type="text" placeholder="Search..." @input="onInput" />
-                <i class="search link icon"></i>
-            </div>
+      <div class="item">
+        <div class="ui transparent icon input">
+          <input type="text" placeholder="Search..." @input="onInput" />
+          <i class="search link icon"></i>
         </div>
+      </div>
     </div>
-
   </div>
 </template>
 
@@ -26,4 +28,12 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.ui.header {
+    margin-left: 10px;
+    margin-bottom: 0;
+}
+.users.icon, .header .content {
+    vertical-align: bottom;
+}
+</style>
