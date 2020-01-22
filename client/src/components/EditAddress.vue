edd972d07af78754bc1cbcdd134ca2383988f346
Admin <patrickemaher@yahoo.com>
2020-01-21 20:53:14 -0800

    Moving the states dropdown to a separate json file

--- client/src/components/EditAddress.vue
+++ client/src/components/EditAddress.vue
@@ -45,57 +45,8 @@
                     <select class="ui fluid dropdown" v-model="editingAddress.state"
                         @change="$v.editingAddress.state.$touch();">
                         <option value="">State</option>
-                        <option value="AL">Alabama</option>
-                        <option value="AK">Alaska</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="CA">California</option>
-                        <option value="CO">Colorado</option>
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="DC">District Of Columbia</option>
-                        <option value="FL">Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="HI">Hawaii</option>
-                        <option value="ID">Idaho</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IN">Indiana</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NV">Nevada</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="NY">New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="OH">Ohio</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="OR">Oregon</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TN">Tennessee</option>
-                        <option value="TX">Texas</option>
-                        <option value="UT">Utah</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WA">Washington</option>
-                        <option value="WV">West Virginia</option>
-                        <option value="WI">Wisconsin</option>
-                        <option value="WY">Wyoming</option>
+                        <option v-for="data in $options.stateJson" :key="data.abbreviation" 
+                                :value="data.abbreviation">{{data.name}}</option>
                     </select>
                     <div class="ui red pointing prompt label" v-if="!$v.editingAddress.state.required">State is required.</div>
                 </div>
@@ -136,6 +87,7 @@
 import { required, email, minLength, between } from "vuelidate/lib/validators";
 import { mapActions, mapGetters } from 'vuex';
 import { TheMask } from 'vue-the-mask';
+import states from './states/states.json';
 const zipcode = (value) => {
     if (!value) return true;
     const regex = /^\d{5}(?:[-\s]\d{4})?$/;
@@ -143,6 +95,7 @@ const zipcode = (value) => {
 }
 export default {
     name: 'EditAddress',
+    stateJson: states,
     methods: {
         ...mapActions(['fetchAddress', 'updateAddress']),
         cancelEdit() {
